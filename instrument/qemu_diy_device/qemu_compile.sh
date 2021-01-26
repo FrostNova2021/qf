df5c3bf543369b964446fc92b68b250eea50fa5d
FrostNova2021 <doxc@qq.com>
2021-01-26 03:39:40 +0000

    add :qemu test device and compile command

---
+++ instrument/qemu_diy_device/qemu_compile.sh
@@ -0,0 +1,5 @@
+cd ~/Desktop/vm_qemu/qemu_fuzzer/instrument
+cp ./sanitize_converage.o ../../qemu-5.0.0/
+cd ~/Desktop/vm_qemu/qemu-5.0.0/
+./configure --cc="/usr/bin/clang-sp" --extra-cflags="-fsanitize-coverage=trace-pc-guard -fPIE"
+make -j12 CFLAGS="/home/fuzzing/Desktop/vm_qemu/qemu-5.0.0/sanitize_converage.o"

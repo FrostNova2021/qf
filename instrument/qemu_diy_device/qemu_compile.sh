1026fe978cdf8d0176eab828c5187878c77a8eaf
FrostNova2021 <doxc@qq.com>
2021-01-31 13:02:58 +0000

    update :qemu compile shell

--- instrument/qemu_diy_device/qemu_compile.sh
+++ instrument/qemu_diy_device/qemu_compile.sh
@@ -2,4 +2,4 @@ cd ~/Desktop/vm_qemu/qemu_fuzzer/instrument
 cp ./sanitize_converage.o ../../qemu-5.0.0/
 cd ~/Desktop/vm_qemu/qemu-5.0.0/
 ./configure --cc="/usr/bin/clang-sp" --extra-cflags="-fsanitize-coverage=trace-pc-guard -fPIE"
-make -j12 CFLAGS="/home/fuzzing/Desktop/vm_qemu/qemu-5.0.0/sanitize_converage.o"
+make -j14 CFLAGS="/home/fuzzing/Desktop/vm_qemu/qemu-5.0.0/sanitize_converage.o -Wunknow-warning-option"

483c4030eb018a58327b7e52ce39f4eeff8d4230
FrostNova2021 <doxc@qq.com>
2021-02-01 22:33:48 -0500

    update :qemu support asan

--- instrument/qemu_diy_device/qemu_compile.sh
+++ instrument/qemu_diy_device/qemu_compile.sh
@@ -1,5 +1,5 @@
 cd ~/Desktop/vm_qemu/qemu_fuzzer/instrument
 cp ./sanitize_converage.o ../../qemu-5.0.0/
 cd ~/Desktop/vm_qemu/qemu-5.0.0/
-./configure --cc="/usr/bin/clang-sp" --extra-cflags="-fsanitize-coverage=trace-pc-guard -fPIE"
-make -j14 CFLAGS="/home/fuzzing/Desktop/vm_qemu/qemu-5.0.0/sanitize_converage.o -Wunknow-warning-option"
+./configure --cc="/usr/bin/clang-sp" --extra-cflags="-fsanitize-coverage=trace-pc-guard -g3 -fsanitize=address -fPIE"
+make -j14 CFLAGS="/home/ubuntu/Desktop/vm_qemu/qemu_fuzzer/instrument/sanitize_converage.o -g3 -Wall -Wno-unused-command-line-argument"

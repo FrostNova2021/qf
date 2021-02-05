b85f5b909bf57ae789eecbdd051c0a1ba7b22520
FrostNova2021 <doxc@qq.com>
2021-02-05 10:20:19 -0500

    add :llvm compiler-rt make

--- instrument/llvm-sanitizer/llvm_compile.sh
+++ instrument/llvm-sanitizer/llvm_compile.sh
@@ -1,1 +1,2 @@
-cmake -DCMAKE_BUILD_TYPE=Release   -DLLVM_INCLUDE_TESTS=OFF -DLLVM_ENABLE_PROJECTS='clang'  ../../llvm-project-llvmorg-11.0.1/llvm
+cmake -DCMAKE_BUILD_TYPE=Release -DLLVM_INCLUDE_TESTS=OFF -DLLVM_ENABLE_PROJECTS='clang'  ../../llvm-project-llvmorg-11.0.1/llvm
+cmake -DCMAKE_BUILD_TYPE=Release -DLLVM_INCLUDE_TESTS=OFF -DLLVM_ENABLE_PROJECTS='compiler-rt' ../../llvm-project-llvmorg-11.0.1/llvm
